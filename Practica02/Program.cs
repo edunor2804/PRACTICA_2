bfac8394f5365988517e04d945eefec607120846
edunor2804 <134438869+edunor2804@users.noreply.github.com>
2026-06-06 18:33:27 -0500

    Se agrego el proyecto EJE1

---
+++ Practica02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando la variable edad como tipo de dato entero
+            int edad;
+
+            //Solicita al usuario que ingrese su edad
+            Console.Write("Ingrese su edad: ");
+
+            //Convierte la edad ingresada y lo convierte a entero
+            edad = int.Parse(Console.ReadLine());
+
+            //Verifica si la edad es mayor a 18
+            if(edad >= 18) 
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else 
+            {
+                Console.WriteLine("Eres menor de edad");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
